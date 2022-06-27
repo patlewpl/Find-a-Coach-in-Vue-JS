45fd303c8c1294743adbb194f5194ffa00f90980
patlewpl <business.patlew@gmail.com>
2022-06-27 22:57:53 +0200

    requests and http

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,1 +1,118 @@
-<template>CONTACT FORM</template>
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control" :class="{ errors: !email.isValid }">
+      <label for="email">Your E-Mail</label>
+      <input
+        type="email"
+        id="email"
+        v-model.trim="email.val"
+        @blur="clearValidity('email')"
+      />
+      <p v-if="!email.isValid">Email cannot be empty.</p>
+    </div>
+    <div class="form-control" :class="{ errors: !message.isValid }">
+      <label for="message">Message</label>
+      <textarea
+        id="message"
+        rows="5"
+        v-model.trim="message.val"
+        @blur="clearValidity('message')"
+      ></textarea>
+      <p v-if="!message.isValid">Message cannot be empty.</p>
+    </div>
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: {
+        val: '',
+        isValid: true,
+      },
+      message: {
+        val: '',
+        isValid: true,
+      },
+      formIsValid: true,
+    };
+  },
+  methods: {
+    clearValidity(input) {
+      this[input].isValid = true;
+    },
+    validateForm() {
+      this.formIsValid = true;
+      if (this.email.val === '' || !this.email.val.includes('@')) {
+        this.email.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.message.val === '') {
+        this.message.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    submitForm() {
+      this.validateForm();
+      if (!this.formIsValid) {
+        return;
+      }
+      const formData = {
+        email: this.email.val,
+        message: this.message.val,
+        coachId: this.$route.params.id,
+      };
+      this.$store.dispatch('requests/contactCoach', formData);
+      this.$router.replace('/coaches');
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
